079ce4a307b04c0a4971f41bc284a56066930e27
israel <israekbaruque992@gmail.com>
2023-09-19 08:42:18 -0300

    abiente preparado

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Parse from "parse/react-native.js";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
+Parse.setAsyncStorage(AsyncStorage);
+
+Parse.initialize('n7j1NCvCyJVoqzTM7ydZKNksa2lMEyZcQO93lhmL','IaBYf19GXvWy5byDBd6EVq8SlM9JHK6a6Swj4rVz');
+Parse.serverURL = 'https://parseapi.back4app.com/';
 export default function App() {
   return (
     <View style={styles.container}>
